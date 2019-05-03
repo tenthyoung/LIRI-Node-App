d4145ab28acb92269cf3de912e072372eb2d95ac
tenthyoung <tenthyoung@gmail.com>
2019-05-02 19:58:12 -0700

    added Spotify keys

--- liri.js
+++ liri.js
@@ -13,4 +13,20 @@ var spotify = new Spotify(keys.spotify);
 // do-what-it-says
 
 
+// 1. node liri.js concert-this <artist/band name here>
+// This will search the Bands in Town Artist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") for an artist and render the following information about each event to the terminal:
+// Name of the venue
+// Venue location
+// Date of the Event (use moment to format this as "MM/DD/YYYY
+
+// 2. node liri.js spotify-this-song '<song name here>'
+// This will show the following information about the song in your terminal/bash window
+// Artist(s)
+// The song's name
+// A preview link of the song from Spotify
+// The album that the song is from
+// If no song is provided then your program will default to "The Sign" by Ace of Base.
+
+
+
 
