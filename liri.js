e7570acafb20a2782edf2721664a7aa3b792b1b8
tenthyoung <tenthyoung@gmail.com>
2019-05-03 12:52:59 -0700

    added switch statement

--- liri.js
+++ liri.js
@@ -1,31 +1,35 @@
-require("dotenv").config();
+// require("dotenv").config();
 
-var keys = require("./keys.js");
+// var keys = require("./keys.js");
 
-var Spotify = require('node-spotify-api');
-var spotify = new Spotify(keys.spotify);
+// var Spotify = require('node-spotify-api');
+// var spotify = new Spotify(keys.spotify);
 
-// Make it so liri.js can take in one of the following commands:
+let command = process.argv[2];
 
-// concert-this
-// spotify-this-song
-// movie-this
-// do-what-it-says
+switch (command) {
+    case "concert-this":
+      console.log("concert");
+      break;
+    
+    case "spotify-this-song":
+      console.log("spotiffy");
+      break;
+    
+    case "movie-this":
+      console.log("moviee time");
+      break;
+    
+    case "do-what-it-says":
+      console.log("Do what it says");
+      break;
+
+    default:
+      console.log("Sorry, I didn't quite get that.");
+}
 
 
-// 1. node liri.js concert-this <artist/band name here>
-// This will search the Bands in Town Artist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") for an artist and render the following information about each event to the terminal:
-// Name of the venue
-// Venue location
-// Date of the Event (use moment to format this as "MM/DD/YYYY
 
-// 2. node liri.js spotify-this-song '<song name here>'
-// This will show the following information about the song in your terminal/bash window
-// Artist(s)
-// The song's name
-// A preview link of the song from Spotify
-// The album that the song is from
-// If no song is provided then your program will default to "The Sign" by Ace of Base.
 
 
 
